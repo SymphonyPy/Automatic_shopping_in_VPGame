d6484368ad05a4d2c6874570ae72f784387c40e3
Symphony0998 <568603742@qq.com>
2017-05-19 18:29:12 +0800

    原网站改动

--- User.py
+++ User.py
@@ -8,12 +8,6 @@ class User(object):
         self.info = self.login(account)
 
     def login(self, account):
-        data = {
-            "Register[username]": account["username"],
-            "Register[password]": account["password"],
-            "Register[rememberMe]": "",
-            "yt0": "登录"
-        }
         user_info = {
             "nickname": "",
             "gold": "",
@@ -22,10 +16,27 @@ class User(object):
             "user_id": ""
         }
         homepage_url = "http://www.vpgame.com/"
-        login_url = "http://passport.vpgame.com/login.html?redirect=http://www.vpgame.com"
-        sent_url = "http://passport.vpgame.com/verify/sent.html"
+        login_url = "http://passport.vpgame.com/account/signin"
+        sent_url = "http://passport.vpgame.com/account/securitycode"
         verify_url = "http://passport.vpgame.com/login/accountverify.html?redirect=http://passport.vpgame.com/login.html?redirect=http://www.vpgame.com"
-        self.session.post(login_url, data=data)
+        data = {
+            "account": account["username"],
+            "password": account["password"],
+            "captcha": ""
+        }
+        headers = {
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+            'Content-Length': '55',
+            'Host': 'passport.vpgame.com',
+            'Origin': 'http://passport.vpgame.com',
+            'Referer': 'http://passport.vpgame.com/signin?redirect=http%3A%2F%2Fwww.vpgame.com%2F',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        self.session.post(login_url, data=data, headers=headers)
         pattern = re.compile(
             '''"nickname":"(.*?)".*?"gold":"(.*?)","level":"(.*?)"}.*?"session_id":"(.*?)","user_id":"(.*?)"''')
         try:
@@ -36,37 +47,35 @@ class User(object):
             print(user_info)
             return user_info
         except:
-            data = {'name': account["username"]}
             headers = {
-                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'Accept': '*/*',
                 'Accept-Encoding': 'gzip, deflate',
                 'Accept-Language': 'zh-CN,zh;q=0.8',
                 'Connection': 'keep-alive',
-                'Content-Length': '21',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Content-Length': '0',
                 'Host': 'passport.vpgame.com',
                 'Origin': 'http://passport.vpgame.com',
-                'Referer': 'http://passport.vpgame.com/login/accountverify.html?redirect=http://passport.vpgame.com/login.html?redirect=http://www.vpgame.com',
+                'Referer': 'http://passport.vpgame.com/signin?redirect=http%3A%2F%2Fwww.vpgame.com%2F',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
                 'X-Requested-With': 'XMLHttpRequest'
             }
-            print(self.session.post(sent_url, data=data, headers=headers).text)
-            data = {'email': account["username"], 'verify-code': ''}
+            print(self.session.post(sent_url, headers=headers).text)
+            data = {'account': account["username"], 'verify_code': ''}
             headers = {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept': '*/*',
                 'Accept-Encoding': 'gzip, deflate',
                 'Accept-Language': 'zh-CN,zh;q=0.8',
                 'Cache-Control': 'max-age=0',
                 'Connection': 'keep-alive',
-                'Content-Length': '43',
-                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': '45',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'Host': 'passport.vpgame.com',
                 'Origin': 'http://passport.vpgame.com',
-                'Referer': 'http://passport.vpgame.com/login/accountverify.html?redirect=http://passport.vpgame.com/login.html?redirect=http://www.vpgame.com',
-                'pgrade-Insecure-Requests': '1',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
+                'Referer': 'http://passport.vpgame.com/signin?redirect=http%3A%2F%2Fwww.vpgame.com%2F',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+                "X - Requested - With": "XMLHttpRequest"
             }
-            data['verify-code'] = input("Verify Code:")
+            data['verify_code'] = input("Verify Code:")
             self.session.post(verify_url, data=data, headers=headers)
             html = self.session.get(homepage_url).text
             for i, j in zip(["nickname", "gold", "level", "session_id", "user_id"],
